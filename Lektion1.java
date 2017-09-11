734684e6ea61402d3bfd717fbbba060e9d483de7
Rasmus Mattas <mara16@nti.umea>
2017-09-11 11:00:53 +0200

    Slut Lektion1

--- Lektion1.java
+++ Lektion1.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class Lektion1{
 	public static void main(String[] args){
-            JOptionPane.showMessageDialog(null, "Du k" + '\u00F6' + "r " + System.getProperty("os.name") + " idag!");
-            Toolkit.getDefaultToolkit() .beep();
-            JOptionPane.showMessageDialog(null, "0738003244");
-            Toolkit.getDefaultToolkit() .beep();
+            //JOptionPane.showMessageDialog(null, "Du k" + '\u00F6' + "r " + System.getProperty("os.name") + " idag!");
+            //Toolkit.getDefaultToolkit() .beep();
+            //JOptionPane.showMessageDialog(null, "0738003244");
+            //Toolkit.getDefaultToolkit() .beep();
+        //JOptionPane.showMessageDialog(null, "Rasmus Mattas\nPerOlsV11\n0738003244");
+        //JOptionPane.showMessageDialog(null, "Datum och tid:\n" + Calendar.getInstance() .getTime());
+        JOptionPane.showMessageDialog(null, "Information", "Information", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Varning", "Varning", JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Fel", "Felmeddelande", JOptionPane.ERROR_MESSAGE);
 	}
 }
