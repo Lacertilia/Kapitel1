9b180579c28b460b56975568c20e1fb915290b96
Rasmus Mattas <mara16@nti.umea>
2017-09-11 10:25:31 +0200

    first

---
+++ Lektion1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Lektion1{
+	public static void main(String[] args){
+            JOptionPane.showMessageDialog(null, "Du k" + '\u00F6' + "r " + System.getProperty("os.name") + " idag!");
+            Toolkit.getDefaultToolkit() .beep();
+            JOptionPane.showMessageDialog(null, "0738003244");
+            Toolkit.getDefaultToolkit() .beep();
+	}
+}
